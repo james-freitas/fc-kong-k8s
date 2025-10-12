b169dbe509e6b5719b23847274c648a19c944d46
James Freitas <jamesfrj@yahoo.com.br>
2025-10-12 18:18:08 -0300

    Update performance test configuration

--- load/create_bet_load.js
+++ load/create_bet_load.js
@@ -1,17 +1,17 @@
-import {check} from 'k6';
-import http from 'k6/http';
+import { check } from "k6";
+import http from "k6/http";
 
-const KONG_CLIENT = 'kong';
-const KONG_SECRET = 'ohSN0VS9Qtdi5KCLmkj52jGFU91FQJn2';
-const USER = 'maria';
-const PASS = 'maria';
+const KONG_CLIENT = "kong";
+const KONG_SECRET = "Cz8AzkHMqOEcot9ZyUsprIArJQvMbLEI";
+const USER = "maria";
+const PASS = "maria";
 
 export const options = {
   stages: [
-    { target: 0, duration: '10s' },
-    { target: 5, duration: '60s' },
-    { target: 5, duration: '60s' },
-    { target: 5, duration: '180s' },
+    { target: 0, duration: "10s" },
+    { target: 5, duration: "60s" },
+    { target: 5, duration: "60s" },
+    { target: 5, duration: "180s" },
   ],
 };
 
@@ -25,35 +25,38 @@ function authenticateUsingKeycloak(clientId, clientSecret, username, pass) {
     scope: "openid",
   };
   const headers = { "Content-Type": "application/x-www-form-urlencoded" };
-  const response = http.post("http://keycloak.iam/realms/bets/protocol/openid-connect/token", formData, { headers });
+  const response = http.post(
+    "http://keycloak.iam/realms/bets/protocol/openid-connect/token",
+    formData,
+    { headers }
+  );
   return response.json();
 }
 
 export function setup() {
-  return authenticateUsingKeycloak(
-      KONG_CLIENT,
-      KONG_SECRET,
-      USER,
-      PASS
-  );
+  return authenticateUsingKeycloak(KONG_CLIENT, KONG_SECRET, USER, PASS);
 }
 
 export default function (data) {
   const params = {
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${data.access_token}`, // or `Bearer ${clientAuthResp.access_token}`
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${data.access_token}`, // or `Bearer ${clientAuthResp.access_token}`
     },
   };
   const payload = JSON.stringify({
-    match: '1X-DC',
-    email: 'joe@doe.com',
+    match: "1X-DC",
+    email: "joe@doe.com",
     championship: "Uefa Champions League",
     awayTeamScore: "2",
-    homeTeamScore: "3"
+    homeTeamScore: "3",
   });
-  let response = http.post("http://kong-kong-proxy.kong/api/bets", payload, params);
+  let response = http.post(
+    "http://kong-kong-proxy.kong/api/bets",
+    payload,
+    params
+  );
   check(response, {
-    'is status 201': (r) => r.status === 201,
+    "is status 201": (r) => r.status === 201,
   });
 }
