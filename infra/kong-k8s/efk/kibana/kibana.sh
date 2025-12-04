97ccb640264c40004c1ed1fdf57e33487402fe02
James Freitas <jamesfrj@yahoo.com.br>
2025-12-04 00:11:56 -0300

    update kibana configuration

--- infra/kong-k8s/efk/kibana/kibana.sh
+++ infra/kong-k8s/efk/kibana/kibana.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-helm install kibana elastic/kibana \
-  --version=7.17.1 \
-  --namespace=logs \
-  --set service.type=NodePort \
-  --set service.nodePort=31000
+helm uninstall kibana -n logs 2>/dev/null || true
+sleep 10
+
+  helm install kibana elastic/kibana \
+    --version=7.17.1 \
+    --namespace=logs \
+    --set service.type=NodePort \
+    --set service.nodePort=31000 \
+    --set resources.requests.cpu="100m" \
+    --set resources.requests.memory="512Mi" \
+    --set resources.limits.cpu="500m" \
+    --set resources.limits.memory="1Gi" \
+    --set elasticsearchHosts="http://elasticsearch-master:9200" \
+    --set elasticsearchUsername="elastic" \
+    --set elasticsearchPassword="changeme" \
+    --set env.NODE_OPTIONS="-Xmx512m -Xms512m"
